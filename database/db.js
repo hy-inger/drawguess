67af8f28b367f9101efb311e7c3be3c33ce0c4a2
huangying312 <jssy010@126.com>
2015-01-22 16:01:49 +0800

    初始化项目。首次将项目提交到github上。

---
+++ database/db.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/testdb');//；连接数据库
+db.connection.on("error",function(error){
+	console.log("数据库连接失败："+error);
+});
+db.connection.on("open",function(){
+	console.log("数据库连接成功！");
+});
+var Schema = mongoose.Schema; // 创建属性模型,但是此模式还未和users集合有关联
+var userScheMa = new Schema({//new Schema()中传入一个JSON对象，该对象形如 xxx:yyyy ,xxx是一个字符串，定义了属性，yyy是一个Schema.Type，定义了属性类型
+	name: String,
+	sid: String,
+	school:String
+}); 
+exports.user = db.model('users', userScheMa); // 与users集合关联
