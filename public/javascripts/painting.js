ac8f7cf90edca05054a75d8f66e3924e950b110a
huangying312 <jssy010@126.com>
2015-02-10 22:38:14 +0800

    初次连接socket。

--- public/javascripts/painting.js
+++ public/javascripts/painting.js
@@ -143,4 +143,6 @@ $(document).ready(function(){
 			}
 		});
 	});
+
+	
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../database/db').user;
+var server = require('http').createServer(router);
+server.listen(80);
+var io = require('socket.io').listen(server);
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -16,6 +19,26 @@ router.get('/room/painting', function(req, res) {
 
 });
 
+io.on('connection',function(socket){
+	socket.emit('open');
+	var client = {
+		socket:socket,
+		name:'huangying',
+		color:'red'
+	};
+	socket.on('message',function(msg){
+		var obj = {time:getTime(),color:client.color,name:client.name};
+		console.log(client.name+":"+msg);
+		socket.emit('message',obj);
+	});
+	socket.on('disconnect',function(){
+		console.log(client.name+':disconnect');
+	});
+});
 
+var getTime=function(){
+  var date = new Date();
+  return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+}
 
 module.exports = router;
